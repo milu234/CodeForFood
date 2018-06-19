da5e953dea219b1fc5a125cdc8f5c91e09d536c3
milu234 <milanhazra234.mh@gmail.com>
2018-06-19 23:48:57 +0530

    Sign in and Sign up Button added Background imagr added

---
+++ app/src/main/java/com/example/milan/codeforfood/SignIn.java
@@ -0,0 +1,15 @@
+package com.example.milan.codeforfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+
+    }
+}
