d0d7bc0dc2bf3416472f622588441f5ea7cf3d91
milu234 <milanhazra234.mh@gmail.com>
2018-06-20 12:10:08 +0530

    Render error cleared

--- app/src/main/java/com/example/milan/codeforfood/MainActivity.java
+++ app/src/main/java/com/example/milan/codeforfood/MainActivity.java
@@ -1,28 +1,9 @@
 package com.example.milan.codeforfood;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    public Button button7;
-
-
-    public void init(){
-        button7 = (Button)findViewById(R.id.button7);
-
-        button7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent toy= new Intent(MainActivity.this,SignIn.class);
-                startActivity(toy);
-            }
-        });
-    }
 
 
 
@@ -30,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
     }
 }
