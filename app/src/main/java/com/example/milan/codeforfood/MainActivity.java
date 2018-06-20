a945d5897d849696cd77c5a5d0308f3802b4d5e7
milu234 <milanhazra234.mh@gmail.com>
2018-06-21 00:31:32 +0530

    Username Password Working

--- app/src/main/java/com/example/milan/codeforfood/MainActivity.java
+++ app/src/main/java/com/example/milan/codeforfood/MainActivity.java
@@ -8,18 +8,11 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button button13;
 
-    public void init(){
-        button13 = (Button)findViewById(R.id.button13);
-        button13.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent toy = new Intent(MainActivity.this,SignIn.class);
-                startActivity(toy);
-            }
-        });
-    }
+
+
+
+
 
 
 
@@ -27,6 +20,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
     }
 }
