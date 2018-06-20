71db5a9447e963d174f882fc65d8b4f2c51120f1
milu234 <milanhazra234.mh@gmail.com>
2018-06-20 21:49:50 +0530

    Sign in and Sign up updated

--- app/src/main/java/com/example/milan/codeforfood/MainActivity.java
+++ app/src/main/java/com/example/milan/codeforfood/MainActivity.java
@@ -1,9 +1,25 @@
 package com.example.milan.codeforfood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button button13;
+
+    public void init(){
+        button13 = (Button)findViewById(R.id.button13);
+        button13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toy = new Intent(MainActivity.this,SignIn.class);
+                startActivity(toy);
+            }
+        });
+    }
 
 
 
@@ -11,5 +27,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
 }
