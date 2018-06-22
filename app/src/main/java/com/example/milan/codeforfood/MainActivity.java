aad86a4c52f63105da8ac9afca5f7654a0ce3575
milu234 <milanhazra234.mh@gmail.com>
2018-06-22 21:52:16 +0530

    Authentication Completed

--- app/src/main/java/com/example/milan/codeforfood/MainActivity.java
+++ app/src/main/java/com/example/milan/codeforfood/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.milan.codeforfood;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    private Button buttonSignIn;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignUp;
+
+    private FirebaseAuth firebaseAuth;
+
+    private ProgressDialog progressDialog;
 
 
 
@@ -20,12 +40,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseAuth =  FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser()!= null){
+            //profile activity here
+           finish();
+            startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+        }
 
         Button buttonLM = (Button)findViewById(R.id.buttonLM);
         Button buttonJTC = (Button)findViewById(R.id.buttonJtC);
 
+        editTextEmail = (EditText)findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText)findViewById(R.id.editTextPassword);
+        buttonSignIn = (Button)findViewById(R.id.buttonSignIn);
+        textViewSignUp = (TextView)findViewById(R.id.textViewSignUp);
+
+        progressDialog = new ProgressDialog(this);
+
+        buttonSignIn.setOnClickListener(this);
+
+       textViewSignUp.setOnClickListener(this);
 
 
+//
         buttonLM.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,16 +71,63 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(int1);
             }
         });
-
+//
         buttonJTC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openActivity();
             }
-        });
-    }
+       });
+   }
     public void  openActivity(){
         Intent intent = new Intent(this,SignUp.class);
         startActivity(intent);
+    }
+
+   private void userLogin(){
+        String email = editTextEmail.getText().toString().trim();
+      String password = editTextPassword.getText().toString().trim();
+
+       if (TextUtils.isEmpty(email)){
+           //email is Empty
+           Toast.makeText(this,"PLease Enter the email",Toast.LENGTH_LONG).show();
+            //stopping the function from executing
+           return;
+       }
+
+        if(TextUtils.isEmpty(password)){
+           //password is Empty
+            Toast.makeText(this,"Please Enter the Password",Toast.LENGTH_LONG).show();
+            return;
+        }
+       progressDialog.setMessage("Logged In");
+       progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+           public void onComplete(@NonNull Task<AuthResult> task) {
+               progressDialog.dismiss();
+                if (task.isSuccessful()){
+                    //start the profile actiity
+                    finish();
+                    startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+
+                }
+            }
+       });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonSignIn){
+            userLogin();
+        }
+
+        if(view == textViewSignUp){
+            finish();
+            startActivity(new Intent(this,SignUp.class));
+        }
     }
 }
--- app/src/main/java/com/example/milan/codeforfood/SignUp.java
+++ app/src/main/java/com/example/milan/codeforfood/SignUp.java
@@ -1,13 +1,22 @@
 package com.example.milan.codeforfood;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener{
 
@@ -16,10 +25,25 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
     private EditText editTextPassword;
     private TextView textViewSignIn;
 
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser()!= null){
+            //profile activity here
+            finish();
+            startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+        }
+
+        progressDialog = new ProgressDialog(this);
 
         buttonRegister = (Button)findViewById(R.id.buttonRegister);
 
@@ -30,6 +54,8 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
 
         buttonRegister.setOnClickListener(this);
         textViewSignIn.setOnClickListener(this);
+
+
     }
 
     private void registerUser(){
@@ -40,21 +66,50 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
             //email is Empty
             Toast.makeText(this,"PLease Enter the email",Toast.LENGTH_SHORT).show();
             //stopping the function from executing
+            return;
         }
 
         if(TextUtils.isEmpty(password)){
             //password is Empty
+            Toast.makeText(this,"Please Enter the Password",Toast.LENGTH_SHORT).show();
+            return;
         }
+        progressDialog.setMessage("Registering User");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
+                if (task.isSuccessful()){
+                    //User is completely register
+                    //we will start the profile activity here
+
+
+                        finish();
+                        startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+
+
+                }else{
+                    Toast.makeText(SignUp.this,"Couldn't Register Please try again",Toast.LENGTH_SHORT).show();
+                }
+                progressDialog.dismiss();
+            }
+        });
+
+        //if the form is valid we will show the progress bar
+
     }
 
     @Override
     public void onClick(View view) {
         if (view == buttonRegister){
-            registerUser;
+            registerUser();
         }
 
         if(view == textViewSignIn){
             //open login
+            startActivity(new Intent(this,MainActivity.class));
         }
 
     }
